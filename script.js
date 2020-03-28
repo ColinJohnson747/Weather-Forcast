b7ac37a71e31d87872172f89ac360ecc190d9477
Colin Johnson <blisk76@gmail.com>
2020-03-28 17:31:19 -0400

    finished script for entering cities to local storage and pulling

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-$(document).ready(function (){
-    
-console.log("Document is Ready.")
+$(document).ready(function() {
+  var searchButton = $("#search");
 
+  console.log("Document is Ready.");
 
+  //for saving cities entered to local storage, and appending them to a list
+  var searchedCities = [];
+  searchButton.click(function() {
+    var citiesInput = document.getElementById("citiesInput").value;
+    $(".citiesList").prepend(
+      `<li class="list-group-item"> ${citiesInput} </li>`
+    );
+    var cities = {
+      id: searchedCities.length,
+      cityName: citiesInput
+    };
+    searchedCities.push(cities);
+    localStorage.setItem("cities", JSON.stringify(searchedCities));
+  });
 
+  //Pulls City names of previously searched cities in local storage
+  function pullCities() {
+    var arrayFromStorage = JSON.parse(localStorage.getItem("cities"));
+    var arrayLength = arrayFromStorage.length;
 
-
-
-
-
-})
+    for (var i = 0; i < arrayLength; i++) {
+      var cityNameFromArray = arrayFromStorage[i].cityName;
+      $(".citiesList").prepend(
+        `<li class="list-group-item"> ${cityNameFromArray} </li>`
+      );
+    }
+  }
+  pullCities();
+});
